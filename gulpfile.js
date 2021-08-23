d2083da6abd899d5dae389268a23f31653561993
thanh thien <thanhthien3046@gmail.com>
2021-08-23 15:10:55 +0900

    commit_sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
 var gulp        = require('gulp')
 var path        = require('path')
-// var fileinclude = require('gulp-file-include')
+var fileinclude = require('gulp-file-include')
 var sass        = require('gulp-sass')(require('sass'));
 
-// gulp.task('fileinclude', function() {
-//     return gulp.src([ "src/pages/*.html" ])
-//         .pipe(fileinclude({
-//             prefix: '@@',
-//             basepath: '@file'
-//         }))
-//        .pipe(gulp.dest(path.join(__dirname, '/dist/pages/')))
-// })
+gulp.task('fileinclude', function() {
+    return gulp.src([ "src/pages/*.html" ])
+        .pipe(fileinclude({
+            prefix: '@@',
+            basepath: '@file'
+        }))
+       .pipe(gulp.dest(path.join(__dirname, '/dist/pages/')))
+})
 
 gulp.task('watch', function () {
-    // gulp.watch('src/pages/*.html', gulp.series('fileinclude'))
-    // gulp.watch('src/partial/*.html', gulp.series('fileinclude'))
+    gulp.watch('src/pages/*.html', gulp.series('fileinclude'))
+    gulp.watch('src/partial/*.html', gulp.series('fileinclude'))
     gulp.watch('src/sass/*.scss', gulp.series('sass'))
 });
 
