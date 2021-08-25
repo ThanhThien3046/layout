04cd7ad4375f9985b1dd1daeda264d80d762b61e
Thanh Thiện <thanhthien3046@gmail.com>
2021-08-26 07:40:40 +0900

    Thien_20212608

--- src/JAVASCRIPT/test.js
+++ src/JAVASCRIPT/test.js
@@ -1,3 +1,4 @@
-const oldArray = [1, 2, 3];
-const newArray = [...oldArray, 4, 5]; //Spread syntax is (... of ...oldArray, 4, 5)
-console.log(newArray);
+// const oldArray = [1, 2, 3];
+// const newArray = [...oldArray, 4, 5]; //Spread syntax is (... of ...oldArray, 4, 5)
+// console.log(newArray);
+console.log('day la dong chu test');
